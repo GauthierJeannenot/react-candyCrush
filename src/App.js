3c2f647857a109c807f0257cf2273947a07441e2
Gauthier Jeannenot <jeannenot.gauthier@gmail.com>
2022-09-19 17:20:39 +0200

    drag and drop, le check du validMove est légérement buggé

--- src/App.js
+++ src/App.js
@@ -13,11 +13,14 @@ const candyColors = [
 function App() {
   //On utilise le hook useState afin de pouvoir définir le set de couleurs de notre tableau de jeu à chaque render
   const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+  //On utilise le hook useState afin de définir le carré pris en main et celui à remplacer
+  const [squareBeingDragged, setSquareBeingDragged] = useState(null)
+  const [squareBeingReplaced, setSquareBeingReplaced] = useState(null)
 
   //On crée une méthode pour checker les couleurs sur une colonne de 3
   const checkForColumnOfThree = () => {
     //On itére jusqu'à l'index du dernier square permettant de faire une colonne de 3 dans notre tableau de jeu (en l'occurence 47)
-    for (let i = 0; i < 47 ; i++) {
+    for (let i = 0; i <= 47 ; i++) {
       
       //On crée notre Array columnOfThree dans lequel on vas stocker l'index de nos colonnes de 3 squares
       const columnOfThree = [i, i+width, i+width*2]
@@ -31,17 +34,20 @@ function App() {
       if (columnOfThree.every(n => currentColorArrangement[n] === decidedColor)) {
         columnOfThree.forEach(n => currentColorArrangement[n] = '')
         //console.log(currentColorArrangement)
+        //On return un booléen si il y'à bien match, ce booléen nous servira à surveiller la condition qu'un drag and drop execute bien une colonne de 3 couleurs
+        return true
       }
     }
   }
   //La même opération pour les colonnes de 3 mais par 4
   const checkForColumnOfFour = () => {
-    for (let i = 0; i < 39; i++) {
+    for (let i = 0; i <= 39; i++) {
       const columnOfFour = [i, i+width, i+width*2, i+width*3];
       const decidedColor = currentColorArrangement[i]
 
       if (columnOfFour.every(n => currentColorArrangement[n] === decidedColor)) {
         columnOfFour.forEach(n => currentColorArrangement[n] = '')
+        return true
       }
     }
   }
@@ -59,6 +65,7 @@ function App() {
       if (notValid.includes(i)) continue
       if (rowOfThree.every(n => currentColorArrangement[n] === decidedColor)) {
         rowOfThree.forEach(n => currentColorArrangement[n] = '')
+        return true
       }
     }
   }
@@ -72,13 +79,13 @@ function App() {
       if(notValid.includes(i)) continue
       if (rowOfFour.every(n => currentColorArrangement[n] === decidedColor)) {
         rowOfFour.forEach(n => currentColorArrangement[n = ''])
+        return true
       }
     }
   }
 
   const moveIntoSquareBelow = () => {
-    //On vas checké le carré sous l'index visé on arréte donc l'itération à l'avant derniére ligne
-    for (let i=0; i < 64 - width; i++) {
+    for (let i=0; i <= 55; i++) {
       const firstRow = [0 ,1 ,2 ,3 ,4 ,5 ,6, 7]
       const isFirstRow = firstRow.includes(i)
       //Si On est au premier rang et que la couleur indiquée est vide
@@ -98,6 +105,70 @@ function App() {
     }
   }
 
+  const dragStart = (e) => {
+    //console.log('start')
+    //On récupére L'id du carré que l'on veux bouger
+    //console.log(e.target)
+    setSquareBeingDragged(e.target)
+    
+  }
+
+  const dragDrop = (e) => {
+    //console.log('drop')
+    //On récupére l'Id du carré sur lequel on vas se déplacer
+    //console.log(e.target)
+    setSquareBeingReplaced(e.target)
+  }
+
+  const dragEnd = () => {
+    //console.log('end')
+    //console.log(e.target)
+
+    //On stock l'id du carré à remplacer, getAttribute('data-id') renvoyant une String il faut le parseInt afin d'obtenir un Int
+    const squareBeingReplacedId = parseInt(squareBeingReplaced.getAttribute('data-id')) 
+    //On stock l'id du carré que l'on déplace, getAttribute('data-id') renvoyant une String il faut le parseInt afin d'obtenir un Int
+    const squareBeingDraggedId = parseInt(squareBeingDragged.getAttribute('data-id'))
+
+    //On récupére l'index de notre carré à remplacer dans notre currentColorArrangement, et on redéfini son background selon le background du carré en déplacement
+    currentColorArrangement[squareBeingReplacedId] = squareBeingDragged.style.backgroundColor
+    //Même opération pour changer la couleur du carré en mouvement
+    currentColorArrangement[squareBeingDraggedId] = squareBeingReplaced.style.backgroundColor
+
+    //console.log('squareBeingDraggedId', squareBeingDraggedId)
+    //console.log('squareBeingReplacedId', squareBeingReplacedId)
+
+    //On veux que le drag & drop ne fonctionne que si il s'effectue selon une liste de conditions :
+    //D'abord on vas vérifier que l'id du squareBeingReplaced correspond à l'id d'un des squares adjacent au squareBeingDragged
+    const validMoves = [
+      squareBeingDraggedId -1,
+      squareBeingDraggedId - width,
+      squareBeingDraggedId +1,
+      squareBeingDraggedId + width
+    ]
+
+    const validMove = validMoves.includes(squareBeingReplacedId)
+
+    //Puis on vérifie si le mouvement crée bien une colonne / ligne de 3/4 fois la même couleur grâce au booléen returné
+    //On stock le booléen dans une variable
+    const isAColumnOfFour = checkForColumnOfFour()
+    const isARowOfFour = checkForRowOfFour()
+    const isAColumnOfThree = checkForColumnOfThree()
+    const isARowOfThree = checkForRowOfThree()
+    //Si l'id du carré existe, que c'est un validMove et que le check pour une colonne ou une ligne de 3 ou 4 couleurs return true alors on reset les setter,
+    //Sinon on annule le Mouvement et on reset le currentColorArrangement grâce à la spreadSyntax
+    if (squareBeingReplacedId &&
+       validMove &&
+       (isAColumnOfFour || isARowOfFour || isAColumnOfThree || isARowOfThree)) {
+        setSquareBeingDragged(null)
+        setSquareBeingReplaced(null)
+    } else {
+      //Ici on redonne aux carré leurs couleurs d'origine, afin d'annuler le mouvement
+      currentColorArrangement[squareBeingReplacedId] = squareBeingReplaced.style.backgroundColor
+      currentColorArrangement[squareBeingDraggedId] = squareBeingDragged.style.backgroundColor
+      setCurrentColorArrangement([...currentColorArrangement])
+    }
+  }
+
 
   const createBoard = () => {
     //On crée un Array randomColorArrangement qui contiendra 64 couleurs tirées aléatoirements de l'Array candyColors
@@ -114,7 +185,7 @@ function App() {
     //On check notre Array randomColorArrangement qui, une fois la boucle finie, doit contenir 64 couleurs aléatoirement réparties depuis notre Array candyColors
     //console.log(randomColorArrangement)
 
-    //A chaque render de l'application, notre Array currentColorArrangement sera donc remplis de 64 couleurs aléatoires
+    //A chaque appel de createBoard(), notre Array currentColorArrangement sera donc remplis de 64 couleurs aléatoires
     setCurrentColorArrangement(randomColorArrangement)
   }
 
@@ -141,7 +212,7 @@ function App() {
       //On utilise la "spreadSyntax" pour créer un nouvel Array auquel on aura appliquer à chaque index le call d'une fonction (en l'occurence notre setter)
       //Simplement passer le nouveau currentColorArrangement ne suffit pas
       setCurrentColorArrangement([...currentColorArrangement])
-    }, 1000)
+    }, 500)
     //Afin de bien clear notre interval on return clearInterval() depuis le hook useEffect en lui passant notre variable gameLoop en argument
     return () => clearInterval(gameLoop)
 
@@ -154,17 +225,28 @@ function App() {
 
   
 
-  // On utilise la méthode map pour attribuer une balise <img> à chaque couleur de notre Array currentColorArrangement, 
+  // On utilise la méthode map pour attribuer une balise <img> à chaque couleur de notre Array currentColorArrangement
   // lui donner une key correspondant à son index et un alt pour la rendre plus "accessible"
   // Et en Attendant d'y ajouter des images un background correspondant à la couleur à l'index donné
+  // Un id correspondant à l'index sur le tableau
+  // On rend le square draggable et on supprime le comportement par défault pour le customiser
   return (
     <div className="app">
       <div className="game">
+      
         {currentColorArrangement.map((candyColor, index) => (
           <img
               key={index}
               style={{backgroundColor: candyColor}}
               alt={candyColor}
+              data-id = {index}
+              draggable={true}
+              onDragStart={dragStart}
+              onDragOver={e => e.preventDefault()}
+              onDragEnter={e => e.preventDefault()}
+              onDragLeave={e => e.preventDefault()}
+              onDrop={dragDrop}
+              onDragEnd={dragEnd}
           />
         ))}
 
